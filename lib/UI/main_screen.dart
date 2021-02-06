81b88f2e8c2e18a7f6d4a0ef4a791f4c4b098a99
Wilson Nicholas <wilzznic05@gmail.com>
2021-02-06 10:49:53 +0700

    Now shows restaurants

--- lib/UI/main_screen.dart
+++ lib/UI/main_screen.dart
@@ -3,6 +3,7 @@ import 'package:restaurant_finder_new/BLoC/bloc_provider.dart';
 import 'package:restaurant_finder_new/BLoC/location_bloc.dart';
 import 'package:restaurant_finder_new/DataLayer/location.dart';
 import 'package:restaurant_finder_new/UI/location_screen.dart';
+import 'package:restaurant_finder_new/UI/restaurant_screen.dart';
 
 class MainScreen extends StatelessWidget {
   @override
@@ -11,11 +12,10 @@ class MainScreen extends StatelessWidget {
       stream: BlocProvider.of<LocationBloc>(context).locationStream,
       builder: (context, snapshot) {
         final location = snapshot.data;
-
         if(location == null) {
           return LocationScreen();
         }
-        return Container();
+        return RestaurantScreen(location: location);
       }
     );
   }
